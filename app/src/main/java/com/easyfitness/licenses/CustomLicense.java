6f2eac0b955d9c2852298eaba17ebfa85caf0ee2
brodeurlv <brodeur.levrai@gmail.com>
2017-09-05 14:34:33 +0200

    Added Icons for Body Measure

---
+++ app/src/main/java/com/easyfitness/licenses/CustomLicense.java
@@ -0,0 +1,61 @@
+package com.easyfitness.licenses;
+
+import android.content.Context;
+
+import com.easyfitness.R;
+
+import de.psdev.licensesdialog.licenses.License;
+
+/**
+ * Created by ccombes on 17/09/05.
+ */
+
+public class CustomLicense extends License {
+
+    private static final long serialVersionUID = 5165684351346813168L;
+    private String mLicenseName = "";
+    private String mLicenseURL = "";
+    private String mLicenseVersion = "";
+
+    public CustomLicense(String pName, String pURL, String pVersion) {
+        mLicenseName = pName;
+        mLicenseURL = pURL;
+        mLicenseVersion = pVersion;
+    }
+
+    @Override
+    public String getName() {
+        return mLicenseName;
+    }
+
+    public void setName(String pName) {
+        mLicenseName = pName;
+    }
+
+    @Override
+    public String readSummaryTextFromResources(final Context context) {
+        return "";
+    }
+
+    @Override
+    public String readFullTextFromResources(final Context context) {
+        return "";
+    }
+
+    @Override
+    public String getVersion() {
+        return "";
+    }
+
+    @Override
+    public String getUrl() {
+        return mLicenseURL;
+    }
+
+    public void setUrl(String pURL) {
+        mLicenseURL = pURL;
+    }
+
+
+}
+
