99a425f3fab0998a9dbba1ad2d54404880145dbb
Taco <SkytkRSfan3895@gmail.com>
2019-04-15 18:35:44 -0400

    Update javacsv 2.0 to 2.1, add java 1.8 compat, reformat gradles and chronometer file
    Signed-off-by: Taco <SkytkRSfan3895@gmail.com>

--- app/src/main/java/gr/antoniom/chronometer/Chronometer.java
+++ app/src/main/java/gr/antoniom/chronometer/Chronometer.java
@@ -9,13 +9,14 @@ import android.content.Context;
 import android.os.Handler;
 import android.os.Message;
 import android.os.SystemClock;
+import android.support.v7.widget.AppCompatTextView;
 import android.util.AttributeSet;
 
 import java.text.DecimalFormat;
 
-public class Chronometer extends android.support.v7.widget.AppCompatTextView {
+public class Chronometer extends AppCompatTextView {
     @SuppressWarnings("unused")
-	private static final String TAG = "Chronometer";
+    private static final String TAG = "Chronometer";
     private static final int TICK_WHAT = 2;
     private long mBase;
     private boolean mVisible;
@@ -38,17 +39,17 @@ public class Chronometer extends android.support.v7.widget.AppCompatTextView {
             }
         }
     };
-    
+
     public Chronometer(Context context) {
-        this (context, null, 0);
+        this(context, null, 0);
     }
 
     public Chronometer(Context context, AttributeSet attrs) {
-        this (context, attrs, 0);
+        this(context, attrs, 0);
     }
 
     public Chronometer(Context context, AttributeSet attrs, int defStyle) {
-        super (context, attrs, defStyle);
+        super(context, attrs, defStyle);
 
         init();
     }
@@ -100,25 +101,25 @@ public class Chronometer extends android.support.v7.widget.AppCompatTextView {
 
     @Override
     protected void onDetachedFromWindow() {
-        super .onDetachedFromWindow();
+        super.onDetachedFromWindow();
         mVisible = false;
         updateRunning();
     }
 
     @Override
     protected void onWindowVisibilityChanged(int visibility) {
-        super .onWindowVisibilityChanged(visibility);
+        super.onWindowVisibilityChanged(visibility);
         mVisible = visibility == VISIBLE;
         updateRunning();
     }
 
     private synchronized void updateText(long now) {
         timeElapsed = now - mBase;
-        
+
         DecimalFormat df = new DecimalFormat("00");
-        
-        int hours = (int)(timeElapsed / (3600 * 1000));
-        int remaining = (int)(timeElapsed % (3600 * 1000));
+
+        int hours = (int) (timeElapsed / (3600 * 1000));
+        int remaining = (int) (timeElapsed % (3600 * 1000));
 
         int minutes = remaining / (60 * 1000);
         remaining = remaining % (60 * 1000);
@@ -130,9 +131,9 @@ public class Chronometer extends android.support.v7.widget.AppCompatTextView {
         if (mPreciseClock) {
             milliseconds = ((int) timeElapsed % 1000) / 100;
         }
-        
+
         String text = "";
-        
+
         if (hours > 0) {
             text += df.format(Math.abs(hours)) + ":";
         }
@@ -140,7 +141,7 @@ public class Chronometer extends android.support.v7.widget.AppCompatTextView {
         text += df.format(Math.abs(minutes)) + ":";
         text += df.format(Math.abs(seconds));
         if (mPreciseClock) text += ":" + Integer.toString(milliseconds);
-        
+
         setText(text);
     }
 
@@ -150,12 +151,13 @@ public class Chronometer extends android.support.v7.widget.AppCompatTextView {
             if (running) {
                 updateText(SystemClock.elapsedRealtime());
                 dispatchChronometerTick();
-                if (mPreciseClock)
+                if (mPreciseClock) {
                     mHandler.sendMessageDelayed(Message.obtain(mHandler,
                             TICK_WHAT), 100);
-                else
+                } else {
                     mHandler.sendMessageDelayed(Message.obtain(mHandler,
                             TICK_WHAT), 1000);
+                }
             } else {
                 mHandler.removeMessages(TICK_WHAT);
             }
@@ -169,13 +171,13 @@ public class Chronometer extends android.support.v7.widget.AppCompatTextView {
         }
     }
 
-	public long getTimeElapsed() {
-		return timeElapsed;
-	}
+    public long getTimeElapsed() {
+        return timeElapsed;
+    }
 
     public interface OnChronometerTickListener {
 
         void onChronometerTick(Chronometer chronometer);
     }
-    
+
 }
