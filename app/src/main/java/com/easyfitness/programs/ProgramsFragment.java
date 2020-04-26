176a943a5d93a181958ea25ea011334479d897e1
senpl <urbanekmike@gmail.com>
2020-04-26 13:39:25 +0200

    now in kotlin

--- app/src/main/java/com/easyfitness/programs/ProgramsFragment.java
+++ app/src/main/java/com/easyfitness/programs/ProgramsFragment.java
@@ -1,204 +1,197 @@
-package com.easyfitness.programs;
-
-import android.database.Cursor;
-import android.os.Bundle;
-import android.text.Editable;
-import android.text.TextWatcher;
-import android.view.LayoutInflater;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemSelectedListener;
-import android.widget.AutoCompleteTextView;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ListView;
-import android.widget.Toast;
-
-import com.easyfitness.DAO.DAOProgram;
-import com.easyfitness.DAO.Profile;
-import com.easyfitness.DAO.Program;
-import com.easyfitness.MainActivity;
-import com.easyfitness.R;
-import com.easyfitness.machines.MachineCursorAdapter;
-//import com.easyfitness.machines.ExerciseDetailsPager;
-import java.util.ArrayList;
-import androidx.fragment.app.Fragment;
-
-public class ProgramsFragment extends Fragment {
-    private ListView programsList = null;
-    private AutoCompleteTextView searchField = null;
-    private ProgramCursorAdapter mTableAdapter;
-    private EditText programNewName = null;
-    private DAOProgram daoProgram = null;
-    private TextWatcher onTextChangeListener = new TextWatcher() {
-        @Override
-        public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-        }
-
-        @Override
-        public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-
-            if (charSequence.length() == 0) {
-                mTableAdapter.notifyDataSetChanged();
-                mTableAdapter = ((ProgramCursorAdapter) programsList.getAdapter());
-                refreshData();
-            } else {
-                if (mTableAdapter != null) {
-                    mTableAdapter.getFilter().filter(charSequence);
-                    mTableAdapter.notifyDataSetChanged();
-                }
-            }
-        }
-
-        @Override
-        public void afterTextChanged(Editable editable) {
-        }
-    };
-
-    //    private OnItemClickListener onClickListItem = (parent, view, position, id) -> {
-//        // Get Machine Name selected
-//        TextView textViewID = view.findViewById(R.id.LIST_Program_ID);
-//        long machineId = Long.parseLong(textViewID.getText().toString());
+//package com.easyfitness.programs;
 //
-//        ProgramDetailsPager machineDetailsFragment = ProgramDetailsPager.newInstance(machineId, ((MainActivity) Objects.requireNonNull(getActivity())).getCurrentProfil().getId());
-//        FragmentTransaction transaction = getActivity().getSupportFragmentManager().beginTransaction();
-//        // Replace whatever is in the fragment_container view with this fragment,
-//        // and add the transaction to the back stack so the user can navigate back
-//        transaction.replace(R.id.fragment_container, machineDetailsFragment, MainActivity.MACHINESDETAILS);
-//        transaction.addToBackStack(null);
-//        // Commit the transaction
-//        transaction.commit();
+//import android.database.Cursor;
+//import android.os.Bundle;
+//import android.text.Editable;
+//import android.text.TextWatcher;
+//import android.view.LayoutInflater;
+//import android.view.MenuItem;
+//import android.view.View;
+//import android.view.ViewGroup;
+//import android.widget.AdapterView;
+//import android.widget.AdapterView.OnItemSelectedListener;
+//import android.widget.AutoCompleteTextView;
+//import android.widget.Button;
+//import android.widget.EditText;
+//import android.widget.ListView;
+//import android.widget.TextView;
+//import android.widget.Toast;
+//
+//import com.easyfitness.DAO.DAOProgram;
+//import com.easyfitness.DAO.Profile;
+//import com.easyfitness.MainActivity;
+//import com.easyfitness.R;
+//
+//import org.jetbrains.annotations.NotNull;
+//
+//import java.util.Objects;
+//
+//import androidx.databinding.DataBindingUtil;
+//import androidx.fragment.app.Fragment;
+//import androidx.fragment.app.FragmentTransaction;
+//
+//public class ProgramsFragment extends Fragment {
+//    private ListView programsList = null;
+//    private AutoCompleteTextView searchField = null;
+//    private ProgramCursorAdapter mTableAdapter;
+//    private EditText programNewName = null;
+//    private DAOProgram daoProgram = null;
+//    private TextWatcher onTextChangeListener = new TextWatcher() {
+//        @Override
+//        public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+//        }
+//
+//        @Override
+//        public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+//
+//            if (charSequence.length() == 0 ) {
+//                mTableAdapter.notifyDataSetChanged();
+//                mTableAdapter = ((ProgramCursorAdapter) programsList.getAdapter());
+//                refreshData();
+//            } else {
+//                if (mTableAdapter != null) {
+//                    mTableAdapter.getFilter().filter(charSequence);
+//                    mTableAdapter.notifyDataSetChanged();
+//                }
+//            }
+//        }
+//
+//        @Override
+//        public void afterTextChanged(Editable editable) {
+//        }
 //    };
-    private View.OnClickListener clickAddButton = v -> {
-        String programName = programNewName.getText().toString();
-        if (programName.isEmpty()) {
-            Toast.makeText(getContext(), "Enter not empty program name", Toast.LENGTH_LONG).show();
-        } else {
-            DAOProgram lDAOProgram = new DAOProgram(getContext());
-            lDAOProgram.addRecord(programName);
-            programNewName.setText("");
-            mTableAdapter.notifyDataSetChanged();
-            refreshData();
-            Toast.makeText(getContext(), "Added to program list", Toast.LENGTH_LONG).show();
-        }
-    };
-    private OnItemSelectedListener onItemSelectedList = new OnItemSelectedListener() {
-
-        @Override
-        public void onItemSelected(AdapterView<?> parent, View view,
-                                   int position, long id) {
-            //refreshData();
-        }
-
-        @Override
-        public void onNothingSelected(AdapterView<?> parent) {
-        }
-    };
-
-    public static ProgramsFragment newInstance(String name, int id) {
-        ProgramsFragment f = new ProgramsFragment();
-
-        // Supply index input as an argument.
-        Bundle args = new Bundle();
-        args.putString("name", name);
-        args.putInt("id", id);
-        f.setArguments(args);
-
-        return f;
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-
-        // activates onCreateOptionsMenu in this fragment
-        setHasOptionsMenu(true);
-
-        // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.tab_programs, container, false);
-
-        programNewName = view.findViewById(R.id.new_program_name);
-
-        Button addButton = view.findViewById(R.id.addExercise);
-        addButton.setOnClickListener(clickAddButton);
-
-        searchField = view.findViewById(R.id.searchField);
-        searchField.addTextChangedListener(onTextChangeListener);
-        programsList = view.findViewById(R.id.listProgram);
-
-//        machineList.setOnItemClickListener(onClickListItem);
-        daoProgram = new DAOProgram(view.getContext());
-        return view;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle presses on the action bar items
-
-        //for add Programs menu
-        int addId = 555;
-        if (item.getItemId() == addId) {
-            clickAddButton.onClick(getView());
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    public void onStart() {
-        super.onStart();
-        refreshData();
-
-        // for resetting the search field at the start:
-        searchField.setText("");
-
-        programsList.setOnItemSelectedListener(onItemSelectedList);
-    }
-
-    public String getName() {
-        return Objects.requireNonNull(getArguments()).getString("name");
-    }
-
-    public ProgramsFragment getThis() {
-        return this;
-    }
-
-    private void refreshData() {
-        Cursor c;
-        Cursor oldCursor;
-
-        View fragmentView = getView();
-        if (fragmentView != null) {
-            if (getProfil() != null) {
-
-                c = daoProgram.getAllPrograms();
-                if (c == null || c.getCount() <= 0) {
-                    //Toast.makeText(getActivity(), "No records", Toast.LENGTH_SHORT).show();
-                    programsList.setAdapter(null);
-                } else {
-                    if (programsList.getAdapter() == null) {
-                        mTableAdapter = new ProgramCursorAdapter(getActivity(), c, 0, daoProgram);
-                        programsList.setAdapter(mTableAdapter);
-                    } else {
-                        mTableAdapter = ((ProgramCursorAdapter) programsList.getAdapter());
-                        oldCursor = mTableAdapter.swapCursor(c);
-                        if (oldCursor != null) oldCursor.close();
-                    }
-
-                    mTableAdapter.setFilterQueryProvider(constraint -> daoProgram.getFilteredPrograms(constraint));
-                }
-            }
-        }
-    }
-
-    @Override
-    public void onHiddenChanged(boolean hidden) {
-        if (!hidden) refreshData();
-    }
-
-    private Profile getProfil() {
-        return ((MainActivity) Objects.requireNonNull(getActivity())).getCurrentProfil();
-    }
-
-}
+//
+////        private AdapterView.OnItemClickListener onClickListItem = (parent, view, position, id) -> {
+////        // Get Machine Name selected
+////        TextView textViewID = view.findViewById(R.id.LIST_Program_ID);
+////        long machineId = Long.parseLong(textViewID.getText().toString());
+////
+////        ProgramDetailsPager programDetailsFragment = ProgramDetailsPager.newInstance(machineId, ((MainActivity) Objects.requireNonNull(getActivity())).getCurrentProfil().getId());
+////        FragmentTransaction transaction = getActivity().getSupportFragmentManager().beginTransaction();
+////        // Replace whatever is in the fragment_container view with this fragment,
+////        // and add the transaction to the back stack so the user can navigate back
+////        transaction.replace(R.id.fragment_container, programDetailsFragment);
+////        transaction.addToBackStack(null);
+////        // Commit the transaction
+////        transaction.commit();
+////    };
+//    private View.OnClickListener clickAddButton = v -> {
+//        String programName = programNewName.getText().toString();
+//        if (programName.isEmpty()) {
+//            Toast.makeText(getContext(), "Enter not empty program name", Toast.LENGTH_LONG).show();
+//        } else {
+//            DAOProgram lDAOProgram = new DAOProgram(getContext());
+//            lDAOProgram.addRecord(programName);
+//            programNewName.setText("");
+//            mTableAdapter.notifyDataSetChanged();
+//            refreshData();
+//            Toast.makeText(getContext(), "Added to program list", Toast.LENGTH_LONG).show();
+//        }
+//    };
+////    private OnItemSelectedListener onItemSelectedList = new OnItemSelectedListener() {
+////
+////        @Override
+////        public void onItemSelected(AdapterView<?> parent, View view,
+////                                   int position, long id) {
+////            //refreshData();
+////        }
+////
+////        @Override
+////        public void onNothingSelected(AdapterView<?> parent) {
+////        }
+////    };
+//
+//    public static ProgramsFragment newInstance(String name, Long id) {
+//        ProgramsFragment f = new ProgramsFragment();
+//
+//        // Supply index input as an argument.
+//        Bundle args = new Bundle();
+//        args.putString("name", name);
+//        args.putLong("profilId", id);
+//        f.setArguments(args);
+//
+//        return f;
+//    }
+//
+//    @Override
+//    public View onCreateView(@NotNull LayoutInflater inflater, ViewGroup container,
+//                             Bundle savedInstanceState) {
+//
+//        // activates onCreateOptionsMenu in this fragment
+//        setHasOptionsMenu(true);
+//
+//        // Inflate the layout for this fragment
+//        View view = inflater.inflate(R.layout.tab_programs, container, false);
+//
+//        programNewName = view.findViewById(R.id.new_program_name);
+//
+//        Button addButton = view.findViewById(R.id.addExercise);
+//        addButton.setOnClickListener(clickAddButton);
+//
+//        searchField = view.findViewById(R.id.searchField);
+//        searchField.addTextChangedListener(onTextChangeListener);
+//        programsList = view.findViewById(R.id.listProgram);
+//
+////        programsList.setOnItemClickListener(onClickListItem);
+//        daoProgram = new DAOProgram(view.getContext());
+//        return view;
+//    }
+//
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        // Handle presses on the action bar items
+//
+//        //for add Programs menu
+//        int addId = 555;
+//        if (item.getItemId() == addId) {
+//            clickAddButton.onClick(getView());
+//        }
+//        return super.onOptionsItemSelected(item);
+//    }
+//
+//    public String getName() {
+//        return getArguments().getString("name");
+//    }
+//
+//    public ProgramsFragment getThis() {
+//        return this;
+//    }
+//
+//    private void refreshData() {
+//        Cursor c;
+//        Cursor oldCursor;
+//
+//        View fragmentView = getView();
+//        if (fragmentView != null) {
+//            if (getProfil() != null) {
+//
+//                c = daoProgram.getAllPrograms();
+//                if (c == null || c.getCount() <= 0) {
+//                    //Toast.makeText(getActivity(), "No records", Toast.LENGTH_SHORT).show();
+//                    programsList.setAdapter(null);
+//                } else {
+//                    if (programsList.getAdapter() == null) {
+//                        mTableAdapter = new ProgramCursorAdapter(getActivity(), c, 0, daoProgram);
+//                        programsList.setAdapter(mTableAdapter);
+//                    } else {
+//                        mTableAdapter = ((ProgramCursorAdapter) programsList.getAdapter());
+//                        oldCursor = mTableAdapter.swapCursor(c);
+//                        if (oldCursor != null) oldCursor.close();
+//                    }
+//
+//                    mTableAdapter.setFilterQueryProvider(constraint -> daoProgram.getFilteredPrograms(constraint));
+//                }
+//            }
+//        }
+//    }
+//
+//    @Override
+//    public void onHiddenChanged(boolean hidden) {
+//        if (!hidden) refreshData();
+//    }
+//
+//    private Profile getProfil() {
+//        return ((MainActivity) requireActivity()).getCurrentProfil();
+//    }
+//
+//}
---
+++ app/src/main/java/com/easyfitness/programs/ProgramsFragment.kt
@@ -0,0 +1,163 @@
+package com.easyfitness.programs
+
+import android.database.Cursor
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.LayoutInflater
+import android.view.MenuItem
+import android.view.View
+import android.view.ViewGroup
+import android.widget.*
+import androidx.fragment.app.Fragment
+import com.easyfitness.DAO.DAOProgram
+import com.easyfitness.DAO.Profile
+import com.easyfitness.MainActivity
+import com.easyfitness.R
+
+class ProgramsFragment : Fragment() {
+    private var programsList: ListView? = null
+    private lateinit var searchField: AutoCompleteTextView
+    private var mTableAdapter: ProgramCursorAdapter? = null
+    private var programNewName: EditText? = null
+    private var daoProgram: DAOProgram? = null
+    private val onTextChangeListener: TextWatcher = object : TextWatcher {
+        override fun beforeTextChanged(charSequence: CharSequence, i: Int, i1: Int, i2: Int) {}
+        override fun onTextChanged(charSequence: CharSequence, i: Int, i1: Int, i2: Int) {
+            if (charSequence.isEmpty()) {
+                mTableAdapter!!.notifyDataSetChanged()
+                mTableAdapter = programsList!!.adapter as ProgramCursorAdapter
+                refreshData()
+            } else {
+                if (mTableAdapter != null) {
+                    mTableAdapter!!.filter.filter(charSequence)
+                    mTableAdapter!!.notifyDataSetChanged()
+                }
+            }
+        }
+
+        override fun afterTextChanged(editable: Editable) {}
+    }
+
+    //        private AdapterView.OnItemClickListener onClickListItem = (parent, view, position, id) -> {
+    //        // Get Machine Name selected
+    //        TextView textViewID = view.findViewById(R.id.LIST_Program_ID);
+    //        long machineId = Long.parseLong(textViewID.getText().toString());
+    //
+    //        ProgramDetailsPager programDetailsFragment = ProgramDetailsPager.newInstance(machineId, ((MainActivity) Objects.requireNonNull(getActivity())).getCurrentProfil().getId());
+    //        FragmentTransaction transaction = getActivity().getSupportFragmentManager().beginTransaction();
+    //        // Replace whatever is in the fragment_container view with this fragment,
+    //        // and add the transaction to the back stack so the user can navigate back
+    //        transaction.replace(R.id.fragment_container, programDetailsFragment);
+    //        transaction.addToBackStack(null);
+    //        // Commit the transaction
+    //        transaction.commit();
+    //    };
+    private val clickAddButton = View.OnClickListener { v: View? ->
+        val programName = programNewName!!.text.toString()
+        if (programName.isEmpty()) {
+            Toast.makeText(context, "Enter not empty program name", Toast.LENGTH_LONG).show()
+        } else {
+            val lDAOProgram = DAOProgram(context)
+            lDAOProgram.addRecord(programName)
+            programNewName!!.setText("")
+            mTableAdapter!!.notifyDataSetChanged()
+            refreshData()
+            Toast.makeText(context, "Added to program list", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+
+        // activates onCreateOptionsMenu in this fragment
+        setHasOptionsMenu(true)
+
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.tab_programs, container, false)
+        programNewName = view.findViewById(R.id.new_program_name)
+        val addButton = view.findViewById<Button>(R.id.addExercise)
+        addButton.setOnClickListener(clickAddButton)
+        searchField = view.findViewById(R.id.searchField)
+        searchField.addTextChangedListener(onTextChangeListener)
+        programsList = view.findViewById(R.id.listProgram)
+
+//        programsList.setOnItemClickListener(onClickListItem);
+        daoProgram = DAOProgram(view.context)
+        return view
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle presses on the action bar items
+
+        //for add Programs menu
+        val addId = 555
+        if (item.itemId == addId) {
+            clickAddButton.onClick(view)
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    val name: String
+        get() = requireArguments().getString("name")
+
+    val `this`: ProgramsFragment
+        get() = this
+
+    private fun refreshData() {
+        val c: Cursor?
+        val oldCursor: Cursor?
+        val fragmentView = view
+        if (fragmentView != null) {
+            if (profil != null) {
+                c = daoProgram!!.allPrograms
+                if (c == null || c.count <= 0) {
+                    //Toast.makeText(getActivity(), "No records", Toast.LENGTH_SHORT).show();
+                    programsList!!.adapter = null
+                } else {
+                    if (programsList!!.adapter == null) {
+                        mTableAdapter = ProgramCursorAdapter(activity, c, 0, daoProgram)
+                        programsList!!.adapter = mTableAdapter
+                    } else {
+                        mTableAdapter = programsList!!.adapter as ProgramCursorAdapter
+                        oldCursor = mTableAdapter!!.swapCursor(c)
+                        oldCursor?.close()
+                    }
+                    mTableAdapter!!.filterQueryProvider = FilterQueryProvider { constraint: CharSequence? -> daoProgram!!.getFilteredPrograms(constraint) }
+                }
+            }
+        }
+    }
+
+    override fun onHiddenChanged(hidden: Boolean) {
+        if (!hidden) refreshData()
+    }
+
+    private val profil: Profile?
+        get() = (requireActivity() as MainActivity).currentProfil
+
+    companion object {
+        //    private OnItemSelectedListener onItemSelectedList = new OnItemSelectedListener() {
+        //
+        //        @Override
+        //        public void onItemSelected(AdapterView<?> parent, View view,
+        //                                   int position, long id) {
+        //            //refreshData();
+        //        }
+        //
+        //        @Override
+        //        public void onNothingSelected(AdapterView<?> parent) {
+        //        }
+        //    };
+        fun newInstance(name: String?, id: Long?): ProgramsFragment {
+            val f = ProgramsFragment()
+
+            // Supply index input as an argument.
+            val args = Bundle()
+            args.putString("name", name)
+            args.putLong("profilId", id!!)
+            f.arguments = args
+            return f
+        }
+    }
+}
