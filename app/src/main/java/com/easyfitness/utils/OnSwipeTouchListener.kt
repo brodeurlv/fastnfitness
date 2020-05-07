4d6887a735c97ca7ee90d327003b9a50c1f63ecf
senpl <urbanekmike@gmail.com>
2020-05-07 06:48:54 +0200

    gestures that works for me

---
+++ app/src/main/java/com/easyfitness/utils/OnSwipeTouchListener.kt
@@ -0,0 +1,75 @@
+package com.easyfitness.utils
+
+import android.content.Context
+import android.view.GestureDetector
+import android.view.MotionEvent
+import android.view.View
+import kotlin.math.abs
+
+open class OnSwipeTouchListener(ctx: Context) : View.OnTouchListener {
+
+    private val gestureDetector: GestureDetector
+
+    companion object {
+
+        private const val SWIPE_THRESHOLD = 40
+        private const val SWIPE_VELOCITY_THRESHOLD = 80
+    }
+
+    init {
+        gestureDetector = GestureDetector(ctx, GestureListener())
+    }
+
+    override fun onTouch(v: View, event: MotionEvent): Boolean {
+        return gestureDetector.onTouchEvent(event)
+    }
+
+    private inner class GestureListener : GestureDetector.SimpleOnGestureListener() {
+
+
+        override fun onDown(e: MotionEvent): Boolean {
+            return true
+        }
+
+        override fun onFling(e1: MotionEvent, e2: MotionEvent, velocityX: Float, velocityY: Float): Boolean {
+            var result = false
+            try {
+                val diffY = e2.y - e1.y
+                val diffX = e2.x - e1.x
+                if (abs(diffX) > abs(diffY)) {
+                    if (abs(diffX) > SWIPE_THRESHOLD && abs(velocityX) > SWIPE_VELOCITY_THRESHOLD) {
+                        if (diffX > 0) {
+                            onSwipeRight()
+                        } else {
+                            onSwipeLeft()
+                        }
+                        result = true
+                    }
+                } else if (abs(diffY) > SWIPE_THRESHOLD && abs(velocityY) > SWIPE_VELOCITY_THRESHOLD) {
+                    if (diffY > 0) {
+                        onSwipeBottom()
+                    } else {
+                        onSwipeTop()
+                    }
+                    result = true
+                }
+            } catch (exception: Exception) {
+                exception.printStackTrace()
+            }
+
+            return result
+        }
+
+
+    }
+
+    open fun onSwipeRight() {
+
+    }
+
+    open fun onSwipeLeft() {}
+
+    open fun onSwipeTop() {}
+
+    open fun onSwipeBottom() {}
+}
