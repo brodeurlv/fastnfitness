d84a8298ac480c521e5c3b7a181162e3da12123b
FirstWithThisName <git@boseck.net>
2023-09-16 13:38:27 +0200

    Showing default image if all deleted

--- app/src/main/java/com/easyfitness/ProgressImagesFragment.java
+++ app/src/main/java/com/easyfitness/ProgressImagesFragment.java
@@ -14,6 +14,7 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
@@ -140,6 +141,10 @@ public class ProgressImagesFragment extends Fragment {
                 appViMo.getProfile().getValue().getId(), imageOffset);
         if (image != null) {
             ImageUtil.setPic(currentProgressImage, image.getFile());
+        } else {
+            currentProgressImage.setImageDrawable(
+                    ContextCompat.getDrawable(getContext(), R.drawable.ic_gym_bench_50dp)
+            );
         }
     }
 
